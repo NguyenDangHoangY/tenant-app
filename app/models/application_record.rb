18ef046e21d817d43693230c76a845a7c65fdc95
y nguyen <y.nguyen@orientsoftware.com>
2022-10-06 15:49:30 +0700

    creat multi db

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,12 @@
 class ApplicationRecord < ActiveRecord::Base
-  primary_abstract_class
+  # primary_abstract_class
+  self.abstract_class = true
+  # ActiveRecord::Base.connected_to(role: :reading, shard: :johndoe) do
+  #   Article.all # get all articles created by John
+  # end
+  connects_to shards: {
+   default: { writing: :primary, reading: :primary },
+   peter: { writing: :primary_peter, reading: :primary_peter },
+   gween: { writing: :primary_gween, reading: :primary_gween },
+ }
 end
--- db/primary_peter_schema.rb
+++ db/primary_peter_schema.rb
@@ -10,21 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_05_101434) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_06_084603) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "authors", force: :cascade do |t|
-    t.string "slug"
-    t.string "name"
-    t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
